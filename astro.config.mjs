9b18609d5465dfc170b0210ea3bf939f7955f031
janudev <alejandromorenocarrasco@gmail.com>
2024-10-20 15:23:32 +0200

    Despliegue en Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import robotsTxt from "astro-robots-txt"
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()]
+  integrations: [tailwind(), robotsTxt()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  })
 })
